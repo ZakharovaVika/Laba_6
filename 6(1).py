0e4c24875c44730bfb2ad570492e9e73631fdb3f
ZakharovaVika <125094864+ZakharovaVika@users.noreply.github.com>
2023-05-20 10:30:10 +0400

    Update 6(1).py

--- 6(1).py
+++ 6(1).py
@@ -1,23 +1,24 @@
 """Вариант 4. Дан одномерный массив. Сформировать все возможные варианты данного массива путем замены отрицательных элементов на нечетных местах модулями."""
-import copy
 import itertools
 from random import randint
-n=5
+n=10
 arr=[0]*n
 for i in range (n):
     arr[i]=randint(-100, 0)
-# Функция, которая возвращает все возможные варианты массива, заменяя отрицательные числа на нечетных позициях их модулями
 def array_variants(arr):
     variants = []
-    negative_indices = [i for i in range(0, len(arr), 2) if arr[i] < 0]
+    negative_indices = [i for i in range(1, len(arr), 2) if arr[i] < 0]
 
     for i in range(len(negative_indices) + 1):
         for combination in itertools.combinations(negative_indices, i):
-            variant = copy.deepcopy(arr)
             for index in combination:
-                variant[index] = abs(variant[index])
-            variants.append(variant)
+                arr[index] = abs(arr[index])
+            variants.append(arr[:])
+            for index in combination:
+                arr[index] = -abs(arr[index])  # возвращаем массив к исходному состоянию
     return variants
+
+
 
 
 # Получаем все возможные варианты
